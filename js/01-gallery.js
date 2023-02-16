9949c2e32f45c9e3ad0953defecb3e6afd326105
oshaleyko59 <oshaleyko@yahoo.com>
2023-02-15 21:00:03 -0600

    ready for check

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,78 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+/*
+Створи галерею з можливістю кліку по її елементах і
+перегляду повного зображення у модальному вікні.
+Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js.
+Додай закриття модального вікна після натискання клавіші Escape(опціонально).
+*/
+
+//find gallery element
+const galleryContainer = document.querySelector(".gallery");
+
+//render gallery content 
+galleryContainer.insertAdjacentHTML(
+	"beforeend",
+	createGalleryMarkup(galleryItems)
+);
+
+//delegate handling clicks on gallery items to gallery container
+galleryContainer.addEventListener("click", onGalleryClick);
+
+//function to create markup for gallery content from galleryItems array and given template
+function createGalleryMarkup(galleryItems) {
+	return galleryItems
+		.map(
+			//Gallery item markup
+			({ preview, original, description }) => `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    /></a></div>`
+		)
+		.join("");
+}
+
+//function to handle click on gallery item and open modal window
+function onGalleryClick(e) {
+  //check if click target is image
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  //get URL of original image
+  const selectedImgURL = e.target.dataset.source;
+
+  //create modal window
+  const modal = basicLightbox.create(
+		`<img width="1280" src="${selectedImgURL}">`,
+		{
+      //on modal show, add eventlistener for ESC key to close modal window
+			onShow: (instance) => {
+				window.addEventListener("keydown", onKeyDown);
+      },
+      //on modal close, remove keyboard eventlistener
+			onClose: (instance) => {
+				window.removeEventListener("keydown", onKeyDown);
+			},
+		}
+	);
+
+  //show original image
+  modal.show();
+
+  //prevent default behaivior on link click (it was photo download?? TODO: find out why)
+  e.preventDefault();
+
+  //function to handle keyboard event for closing modal window on ESC key
+  function onKeyDown(e) {
+		if (e.key === "Escape") {
+			modal.close();
+		}
+	}
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+/*
+Зроби таку саму галерею використовуючи бібліотеку SimpleLightbox,
+яка візьме на себе обробку кліків по зображеннях,
+відкриття і закриття модального вікна, а також гортання зображень за допомогою клавіатури.
 
+Додай підписи до зображень з атрибута alt.
+Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
+*/
 console.log(galleryItems);
+
+//find gallery element
+const galleryContainer = document.querySelector(".gallery");
+
+//render gallery content
+galleryContainer.insertAdjacentHTML(
+	"beforeend",
+	createGalleryMarkup(galleryItems)
+);
+
+//initialize SimpleLightBox
+var lightbox = new SimpleLightbox(".gallery a", {
+  	/* options for source of caption and caption delay ms */
+	captionsData: "alt",
+	captionDelay: 250,
+});
+
+//function to create markup for gallery content from galleryItems array and given template
+function createGalleryMarkup(galleryItems) {
+	return galleryItems
+		.map(
+			//Gallery item markup
+			({ preview, original, description }) =>
+				`<a class="gallery__item" href="${original}">
+						<img
+							class="gallery__image"
+							src="${preview}"
+							alt="${description}"
+						/>
+					</a>`
+		)
+		.join("");
+}
